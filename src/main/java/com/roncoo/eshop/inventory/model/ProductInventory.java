f0d7dfa823b15b6eedacbc62418ee3397ba48f8d
dream <932370912@qq.com>
2021-02-06 13:02:18 +0800

    first commit

---
+++ src/main/java/com/roncoo/eshop/inventory/model/ProductInventory.java
@@ -0,0 +1,30 @@
+package com.roncoo.eshop.inventory.model;
+/**
+ * 库存数量
+ * @author dream
+ *
+ */
+public class ProductInventory {
+	private Long id;
+	private Integer value;
+	private Long productId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getValue() {
+		return value;
+	}
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+	public Long getProductId() {
+		return productId;
+	}
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	
+}
---
+++ src/main/java/com/roncoo/eshop/inventory/service/ProductInventoryService.java
@@ -0,0 +1,18 @@
+package com.roncoo.eshop.inventory.service;
+
+import com.roncoo.eshop.inventory.model.ProductInventory;
+
+/**
+ * 库存service
+ * @author dream
+ *
+ */
+public interface ProductInventoryService {
+	public void add(ProductInventory productInventory);
+	
+	public void update(ProductInventory productInventory);
+	
+	public void delete(Long id);
+	
+	public ProductInventory findById(Long id);
+}
